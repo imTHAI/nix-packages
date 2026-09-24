99836e10eafca32837e0c0cc843c7db4ab0fca81
imTHAI <36070606+imTHAI@users.noreply.github.com>
2026-09-24 11:32:28 +0000

    keka: automated version bump

--- pkgs/keka/package.nix
+++ pkgs/keka/package.nix
@@ -6,11 +6,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "keka";
-  version = "1.6.7";
+  version = "1.6.8";
 
   src = fetchzip {
     url = "https://github.com/aonez/Keka/releases/download/v${finalAttrs.version}/Keka-${finalAttrs.version}.zip";
-    hash = "sha256-6zC8lQLkJ/Qppyo3XwK6fy4f/bLsIg0L+4GwFfiVlvg=";
+    hash = "sha256-sVPoH7owX6VvKKqLrEcZww0zWpmY/pV2+LHtg683gds=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
